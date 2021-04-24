695ce5a069a488016f4648a6e6138ca736eea1dd
philliptc <phill.tc05@gmail.com>
2021-04-24 23:57:16 +0700

    Create ForgotYourPasswordActivity.kt

---
+++ app/src/main/java/com/example/awol/forgot_password/ForgotYourPasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.example.awol.forgot_password
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.awol.R
+
+class ForgotYourPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot_your_password)
+    }
+}
