2b39e30adcb579eb51f43a0c593f7ce9957b0eea
philliptc <phill.tc05@gmail.com>
2021-04-24 23:57:21 +0700

    Create LoginActivity.kt

---
+++ app/src/main/java/com/example/awol/login_signup/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.awol.login_signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.awol.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
