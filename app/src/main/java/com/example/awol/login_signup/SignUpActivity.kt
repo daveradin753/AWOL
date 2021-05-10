8528cd69c71e5a9bb20e7280bef3edc5a9b3ac56
philliptc <phill.tc05@gmail.com>
2021-05-10 21:04:31 +0700

    sep

--- app/src/main/java/com/example/awol/login_signup/SignUpActivity.kt
+++ app/src/main/java/com/example/awol/login_signup/SignUpActivity.kt
@@ -1,9 +1,14 @@
 package com.example.awol.login_signup
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.example.awol.DataObjectUsers
+import com.example.awol.MainActivity
 import com.example.awol.R
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
@@ -32,11 +37,36 @@ class SignUpActivity : AppCompatActivity() {
         etConfirmPasswordSignUp = findViewById(R.id.etConfirmPasswordSignUp)
         etEmail = findViewById(R.id.etEmail)
 
-        database = FirebaseDatabase.getInstance()
         auth = FirebaseAuth.getInstance()
 
-        databaseRefrence = database.getReference("user").child(auth.uid!!).child("name")
-        databaseRefrence.setValue("Dave")
+
 
     }
+    fun writeNewUser(userId: String, name: String, email: String) {
+        val user = DataObjectUsers(name, email)
+
+        database = FirebaseDatabase.getInstance()
+
+        databaseRefrence = database.getReference("users").child(auth.uid!!)
+        databaseRefrence.setValue(user)
+
+    }
+
+    fun createNewUser (userId: String, name: String, email: String, password: String){
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("user", "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    intent = Intent(this, MainActivity::class.java)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("user", "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
 }
