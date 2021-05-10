ee813add985043f748ba02729114214628821364
philliptc <phill.tc05@gmail.com>
2021-05-10 15:29:41 +0700

    sep

--- app/src/main/java/com/example/awol/login_signup/SignUpActivity.kt
+++ app/src/main/java/com/example/awol/login_signup/SignUpActivity.kt
@@ -3,10 +3,15 @@ package com.example.awol.login_signup
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.awol.R
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        var database = FirebaseDatabase.getInstance().reference
+        database.setValue("yuk dicoba")
     }
 }
